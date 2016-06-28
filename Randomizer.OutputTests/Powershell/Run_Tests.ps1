fccae4fac2646ccbca47c19e799c305bd2c16a47
Pawe� Sienko <psienko@objectivity.co.uk>
2016-06-28 21:27:41 +0200

    Added new powershell script to run output tests

---
+++ Randomizer.OutputTests/Powershell/Run_Tests.ps1
@@ -0,0 +1,21 @@
+﻿param(
+[string]$folderPath = "C:\Temp",
+[string]$teamCityTestFullPath = "C:\TeamCity\buildAgent\work\parent\Random.NET\Randomizer.OutputTests\bin\Debug\Randomizer.OutputTests.exe"
+)
+
+if(-Not(Test-Path $folderPath))
+{
+    New-Item $folderPath -ItemType directory
+    Write-Host "New folder '$folderPath' created"
+}
+
+Write-Host "Tests starting ...."
+if(-Not(Test-Path $teamCityTestFullPath))
+{
+     throw [System.IO.FileNotFoundException] "$teamCityTestFullPath not found"
+}
+
+Invoke-Expression $teamCityTestFullPath
+
+Write-Host "Tests finsihed"
+
