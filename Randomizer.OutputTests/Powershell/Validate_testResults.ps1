485d3c5098e4dc5aeb4d3cf5e3ac87feb556576a
PawelSienko <sienko.pawel@gmail.com>
2016-07-11 19:00:51 +0200

    Update powershell script.

--- Randomizer.OutputTests/Powershell/Validate_testResults.ps1
+++ Randomizer.OutputTests/Powershell/Validate_testResults.ps1
@@ -2,9 +2,9 @@ param(
 [string]$folderPathWithExtension = "C:\Temp\*.log"
 )
 
-if(Test-Path $folderPathWithExtension -eq True)
+if(Test-Path $folderPathWithExtension)
 {
     Write-Error 'Folder is not empty. It contains errors.'
-    [System.Environment]::Exit(1)
+    [System.Environment]::Exit(-1)
 }
 [System.Environment]::Exit(0)
